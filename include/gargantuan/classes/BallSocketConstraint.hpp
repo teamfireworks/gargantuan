fd564153fcedccc24efa67c0b4fb2a36a42333bf
Native315 <45272908+native315@users.noreply.github.com>
2026-08-17 10:15:03 -0400

    feat: ballsocketconstraints
    now i think most of the easy to implement ones are out of the way, but i still gotta implement motor constraints (as in linearvelocity, angularvelocity, lineforce, vectorforce, etc)

---
+++ include/gargantuan/classes/BallSocketConstraint.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "gargantuan/classes/generated/BallSocketConstraint.hpp"
+
+namespace gargantuan {
+	class BallSocketConstraint : public Constraint {
+		I_BallSocketConstraint;
+
+		b3JointId CreateJoint(b3WorldId *world, b3BodyId body0, b3BodyId body1) override;
+	};
+}
