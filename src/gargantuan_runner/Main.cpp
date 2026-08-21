e14441f5bae64fd36e22546288573775f3f89915
godmothersfire <znotfireman@gmail.com>
2026-08-21 14:12:54 +0700

    feat: Value.hpp (start of actual work!)

--- src/gargantuan_runner/Main.cpp
+++ src/gargantuan_runner/Main.cpp
@@ -1,6 +1,21 @@
+#include <flecs.h>
+
 #include <iostream>
+#include <string>
+
+struct InstanceClass {
+	std::string ClassName;
+};
 
 int main(int argc, const char *argv[]) {
+	flecs::world ecs;
+
+	ecs.entity("Test").set<InstanceClass>({"Test"});
+
+	ecs.each([](flecs::entity e, InstanceClass &i) {
+		std::cout << "Entity " << e.name() << " of id #" << e << " is of class " << i.ClassName.c_str() << std::endl;
+	});
+
 	std::cout << "Hello, Gargantuan Rewrite!" << std::endl;
 	return 0;
 }
