98697f021d9569d00d4b14b9ec027f65239421b1
Native315 <45272908+native315@users.noreply.github.com>
2026-08-26 15:22:28 -0400

    fix: HingeConstraint.TargetAngle now uses degrees much easier than i thought

--- src/classes/HingeConstraint.cpp
+++ src/classes/HingeConstraint.cpp
@@ -1,5 +1,6 @@
 #include "gargantuan/classes/HingeConstraint.hpp"
 #include <box3d/box3d.h>
+#include <box3d/math_functions.h>
 #include <box3d/types.h>
 
 namespace gargantuan {
@@ -43,7 +44,7 @@ namespace gargantuan {
 		bool isServo = this->ActuatorType == Enums::ActuatorType::Servo;
 		b3RevoluteJoint_EnableSpring(LeJoint, isServo);
 		if (isServo) {
-			b3RevoluteJoint_SetTargetAngle(LeJoint, this->TargetAngle);
+			b3RevoluteJoint_SetTargetAngle(LeJoint, B3_DEG_TO_RAD * this->TargetAngle);
 		}
 	}
 }
