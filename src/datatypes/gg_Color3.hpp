a4129f6d420459032254657184b11f74fca340de
godmothersfire <znotfireman@gmail.com>
2026-08-30 15:33:45 +0700

    feat: codegen as guided by utrain my goat

--- src/datatypes/gg_Color3.hpp
+++ src/datatypes/gg_Color3.hpp
@@ -6,38 +6,9 @@
 
 #pragma once
 
-#include <tuple>
-
-#include <glm/ext.hpp>
-
-// NOTE: Implementing this by hand for now to get a reference for implementing
-// codegen
+#include "datatypes/generated/Color3.hpp"
 
 struct gg_Color3 {
-public:
-  double R, G, B;
-
-  static gg_Color3 fromRGB(double r, double g, double b);
-  static gg_Color3 fromHSV(double &hue, double &saturation, double &value);
-  static gg_Color3 fromOklab(double &lightness, double &a, double &b);
-  static gg_Color3 fromOklch(double &lightness, double &chroma, double &hue);
-
-  std::tuple<double, double, double> ToHSV() const;
-  std::tuple<double, double, double> ToOklab() const;
-  std::tuple<double, double, double> ToOklch() const;
-
-  glm::vec3 PackHSV() const;
-  glm::vec3 PackOklab() const;
-  glm::vec3 PackOklch() const;
-
-  gg_Color3 Add(const gg_Color3 &other) const;
-  gg_Color3 Sub(const gg_Color3 &other) const;
-  gg_Color3 Mul(const gg_Color3 &other) const;
-  gg_Color3 Div(const gg_Color3 &other) const;
-  std::string ToString() const;
-
-private:
-  void ToHSV(double &hue, double &saturation, double &value);
-  void ToOklab(double &lightness, double &a, double &b);
-  void ToOklch(double &lightness, double &chroma, double &hue);
+  GG_COLOR3
+  gg_Color3(const float &r, const float &g, const float &b) noexcept;
 };
